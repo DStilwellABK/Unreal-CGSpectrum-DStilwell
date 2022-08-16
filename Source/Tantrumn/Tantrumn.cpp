ae9d8785851ad089959af541faea4f5d2011e99c
DylanStilwell <109607065+DylanStilwell@users.noreply.github.com>
2022-08-16 11:54:30 -0500

    Fixing UE

--- Source/Tantrumn/Tantrumn.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Tantrumn.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Tantrumn, "Tantrumn" );
